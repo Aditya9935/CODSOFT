95fa18ec32235b9324f09c95f2051437e71bfa43
Aditya9935 <144621586+Aditya9935@users.noreply.github.com>
2023-09-13 22:46:54 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,10 @@
+
+AOS.init({
+    offset: 250,
+});
+
+var typed = new Typed('#element', {
+    strings: ['Web-Developer', 'Graphic Designer', '<strong>UI/UX</strong> Designer'],
+    typeSpeed: 50,
+    loop: true
+});
